cfaae49951cb89293f91d601f9ac8f696db64ae9
Bert Overduin <boverduin@gmail.com>
2018-02-03 11:48:26 +0000

    Tidy code a bit more

--- pet_shop.rb
+++ pet_shop.rb
@@ -64,7 +64,7 @@ def customer_can_afford_pet(customer, new_pet)
 end
 
 def sell_pet_to_customer(pet_shop, pet, customer)
-  if pet != nil
+  if pet
     if customer_can_afford_pet(customer, pet)
       add_pet_to_customer(customer, pet)
       increase_pets_sold(pet_shop, 1)
